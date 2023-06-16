0570ff952715a98576474dd80ad31338747fecf9
Sumit0397 <123931330+Sumit0397@users.noreply.github.com>
2023-06-16 20:53:08 +0530

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,22 @@
-function ExpenseItem() {
-    return (
-        <>
-            <div><h2>Expense Item</h2>
-                <p>Food  Rs 10</p>
-                <p>Petrol Rs 100</p>
-                <p>MOvies Rs 200</p>
-            </div>
-            <div>
+import './ExpenseItem.css'
 
+function ExpenseItem() {
+    const expenseDate = new Date(2023 , 6 , 16);
+    const expenseItem = "Car Insurance";
+    const expenseAmount = 294.76;
+    const LoactionOfExpenditure = "Car";
+
+    return (
+        
+            <div className='expense-item'>
+                <div>{expenseDate.toISOString()}</div>
+                <div className='expense-item__description'>
+                    <h2>{expenseItem}</h2>
+                    <div className='expense-item__price'>Rs {expenseAmount}</div>
+                    <div>Location of Expenditure = {LoactionOfExpenditure}</div>
+                </div>
             </div>
-        </>
+
     )
 }
 
