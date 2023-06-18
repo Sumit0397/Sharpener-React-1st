99106d6cb8bed751b61cb08259b3c0ad3a4f5a1c
Sumit0397 <123931330+Sumit0397@users.noreply.github.com>
2023-06-18 12:37:08 +0530

    Add files via upload

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+ 
+
+    return (
+        
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date}/>
+                <ExpenseDetails amount={props.amount} title={props.title} location={props.location}/>
+            </Card>
+
+    )
+}
+
+export default ExpenseItem;
