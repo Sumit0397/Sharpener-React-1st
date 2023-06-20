10d4f05c6e11312b3f54290527b0e2c93d9a1c99
Sumit0397 <123931330+Sumit0397@users.noreply.github.com>
2023-06-20 12:07:25 +0530

    Update ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,17 @@ import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
- 
+    const deleteExpenseHandler = () => {
+        const expenseItemElement = document.querySelector('.expense-item');
+        expenseItemElement.remove();
+    }
 
     return (
         
             <Card className='expense-item'>
                 <ExpenseDate date={props.date}/>
                 <ExpenseDetails amount={props.amount} title={props.title} location={props.location}/>
+                <button onClick={deleteExpenseHandler}>Delete Expense</button>
             </Card>
 
     )
