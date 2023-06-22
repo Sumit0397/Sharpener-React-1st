b57bfed45599bc1bf652f16b50a4f76a4631fec5
Sumit0397 <123931330+Sumit0397@users.noreply.github.com>
2023-06-22 23:42:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,75 +1,52 @@
-import ExpenseItem from "./components/Expenses/ExpenseItem";
-import NewExpense from "./components/NewExpense/NewExpense";
-import './Expenses.css';
+import React, { useState } from 'react';
 
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
-  const expenses = [
-    {
-      location: 'Shopping Mall',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { location: 'Tv Showroom', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      location: 'Car Showroom',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      location: 'x Furniture',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  const addExpensehandler = (expenses) =>{
-    console.log("in App.js");
-    console.log(expenses);
-  }
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpensehandler}/>
-      <div className="expenses">
-      
-        {/* <ExpenseItem title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        location={expenses[0].location} />
-      <ExpenseItem title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        location={expenses[1].location} />
-      <ExpenseItem title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        location={expenses[2].location} />
-      <ExpenseItem title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        location={expenses[3].location} /> */}
-        {
-          expenses.map((expense, index) => (
-            <ExpenseItem
-              key={index}
-              title={expense.title}
-              amount={expense.amount}
-              location={expense.location}
-              date={expense.date}
-            />
-
-          ))
-        }
-
-
-      </div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
