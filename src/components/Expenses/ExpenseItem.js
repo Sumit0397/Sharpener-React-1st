ee8295067fa96209575b70a9c4c90ee88a0c1585
Sumit0397 <123931330+Sumit0397@users.noreply.github.com>
2023-06-23 13:03:07 +0530

    Update ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,32 +1,21 @@
-import React , {useState} from 'react';
-import Card from '../UI/Card';
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
-import './ExpenseItem.css'
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-    const [amount , setAmount] = useState(props.amount);
-
-    const updateAmountHandler = () => {
-        setAmount('$100');
-        console.log(amount);
-    }
-    
-    const deleteExpenseHandler = () => {
-        const expenseItemElement = document.querySelector('.expense-item');
-        expenseItemElement.remove();
-    }
-
-    return (
-        
-            <Card className='expense-item'>
-                <ExpenseDate date={props.date}/>
-                <ExpenseDetails amount={amount} title={props.title} location={props.location}/>
-                <button onClick={deleteExpenseHandler}>Delete Expense</button>
-                <button onClick={updateAmountHandler}>Update Amount</button>
-            </Card>
-
-    )
-}
+const ExpenseItem = (props) => {
+  return (
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
+  );
+};
 
 export default ExpenseItem;
